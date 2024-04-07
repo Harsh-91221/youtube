f378b63f388a9c263da6cf16da14fd59cd76fd6a
Harsh91221 <harshch91221@gmail.com>
2024-04-07 21:53:55 +0530

    CODE

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,58 +7,92 @@ import { cacheResults } from '../utils/searchSlice';
 const Header = () => {
     const [searchQuery, setSearchQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
-    const [showsuggestions, setShowSuggestions] = useState(false);
-    const searchCache = useSelector((store) => store.search) || {};
+    const [showSuggestions, setShowSuggestions] = useState(false);
+
+    const searchCache = useSelector((store) => store.search);
     const dispatch = useDispatch();
     useEffect(() => {
         const timer = setTimeout(() => {
-            if (searchCache[searchQuery]) {
+            if (searchCache && searchCache[searchQuery]) {
                 setSuggestions(searchCache[searchQuery]);
-            }
-            else {
-                getSearchSuggestions();
+            } else {
+                getSearchSugsestions();
             }
         }, 200);
+
         return () => {
             clearTimeout(timer);
         };
-    }, [searchQuery]);
-    const getSearchSuggestions = async () => {
-        console.log(searchQuery);
+    }, [searchQuery, searchCache]);
+
+    const getSearchSugsestions = async () => {
         const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
         const json = await data.json();
-        dispatch(cacheResults({
-            [searchQuery]: json[1],
-        }))
+        setSuggestions(json[1]);
+        dispatch(
+            cacheResults({
+                [searchQuery]: json[1],
+            })
+        );
     };
+
     const toggleMenuHandler = () => {
         dispatch(toggleMenu());
-    }
+    };
+
     return (
-        <div className='grid grid-flow-col p-5 m-2 shadow-lg'>
-            <div className='flex col-span-1'>
-                <img onClick={() => toggleMenuHandler()} className='h-8 cursor-pointer' alt='LOGO' src='https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png'></img>
-                <a href='/'>
-                    <img className='h-8 mx-2' alt='LOGO' src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png'></img>
+        <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+            <div className="flex col-span-1">
+                <img
+                    onClick={() => toggleMenuHandler()}
+                    className="h-8 cursor-pointer"
+                    alt="menu"
+                    src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
+                />
+                <a href="/">
+                    <img
+                        className="h-8 mx-2"
+                        alt="youtube-logo"
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+                    />
                 </a>
             </div>
-
-            <div className='col-span-10 px-10'>
+            <div className="col-span-10 px-10">
                 <div>
-                    <input className='px-5 w-1/2 border border-gray-400 p-2 rounded-l-full' type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} onFocus={() => setShowSuggestions(true)} onBlur={() => setShowSuggestions(false)}></input>
-                    <button className='border border-gray-400 p-2 rounded-r-full bg-gray-100'> Search </button>
+                    <input
+                        className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+                        type="text"
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        onFocus={() => setShowSuggestions(true)}
+                        onBlur={() => setShowSuggestions(false)}
+                    />
+                    <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+                        🔍
+                    </button>
                 </div>
-                {showsuggestions && (<div className='fixed bg-white py-2 px-2 w-[32rem] shadow-lg rounded-lg'>
-                    <ul>
-                        {suggestions.map(s => <li key={s} className='px-3 py-2 hover:bg-gray-200'>{s}</li>)}
-                    </ul>
-                </div>)}
+                {showSuggestions && (
+                    <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+                        <ul>
+                            {suggestions.map((s) => (
+                                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                                    🔍 {s}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
-            <div className='col-span-1'>
-                <img className='h-8' alt='user' src='https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png'></img>
+            <div className="col-span-1">
+                <img
+                    className="h-8"
+                    alt="user"
+                    src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+                />
             </div>
         </div>
-    )
-}
+    );
+};
+
 
 export default Header
---
+++ src/components/LiveChat.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import ChatMessage from './ChatMessage'
+import { useDispatch, useSelector } from 'react-redux'
+import { addMessage } from '../utils/chatSlice';
+const LiveChat = () => {
+    const dispatch = useDispatch();
+    const ChatMessages = useSelector((store) => store.chat.messages);
+    useEffect(() => {
+        const i = setInterval(() => {
+            dispatch(addMessage({
+                name: "Harsh Chaudhary",
+                message: "Lorem ipsum dod ike"
+            }))
+        }, 2000);
+        return () => clearInterval(i);
+    }, [])
+    return (
+        <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex-col-reverse'>
+            {ChatMessages.map((c, i) => <ChatMessage key={i} name={c.name} message={c.message} />)}
+        </div>
+    )
+}
+
+export default LiveChat
